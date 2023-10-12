1f1a2588b7fd6b0147fb95abec8765d4d56a4b04
NguyenBang <bangtbtb@gmail.com>
2023-10-12 14:09:22 +0700

    Init commit

---
+++ scripts/build-go.sh
@@ -0,0 +1,16 @@
+cd $( git rev-parse --show-toplevel )
+
+OUT_FOLDER=.
+PACKAGE="$OUT_FOLDER/pbg"
+
+rm -rf $PACKAGE
+mkdir -p $PACKAGE 
+
+# Build  
+protoc -I . --go_out=$OUT_FOLDER --go-grpc_out=$OUT_FOLDER  *.proto
+
+if [[ "$?" == "0" ]];then
+    echo "Build success"
+else
+    echo "Build error." 
+fi 
