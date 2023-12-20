676885888d232cf4286c3420d67f50bc1d7fe484
NguyenBang <bangtbtb@gmail.com>
2023-12-20 16:08:04 +0700

    Add sensor_im.proto, iot_operator.proto

--- scripts/build-go-grpc.sh
+++ scripts/build-go-grpc.sh
@@ -2,38 +2,35 @@ cd $( git rev-parse --show-toplevel )
 
 OUT_DIR="."
 OUT_GRPC="$OUT_DIR/pbg"
-OUT_SWG="$OUT_DIR/swg"
+OUT_SWG="$OUT_DIR/swagger"
 
-rm -rf $OUT_DIR
+# rm -rf $OUT_GRPC
+# rm -rf $OUT_SWG
+
 mkdir -p $OUT_GRPC
 mkdir -p $OUT_SWG
 
 
-protoc -I proto -I proto/libs \
+protoc -I . -I libs \
             --go_out=$OUT_DIR --go-grpc_out=$OUT_DIR  \
             --grpc-gateway_out $OUT_GRPC \
             --grpc-gateway_opt logtostderr=true \
             --grpc-gateway_opt paths=source_relative \
             --grpc-gateway_opt generate_unbound_methods=true \
             --openapiv2_out $OUT_SWG --openapiv2_opt use_go_templates=true \
-            proto/*.proto
-
-# protoc -I proto -I proto/libs \
-#             --go_out=$OUT_DIR --go-grpc_out=$OUT_DIR  \
-#             proto/*.proto
+            *.proto
 
 
-if [[ "$?" == "0" ]];then
-    if [ ! -z "$GOPATH" ]; then
-        echo "Add golang extension code "
-        cp proto/extensions/go/*.ext $OUT_GRPC
-    fi
-
-    cd $OUT_GRPC
-    # pwd 
-    for f in *.go.ext; do
-        mv "$f" "$( basename $f .ext )"
-    done
-else
-    echo "Build error." 
-fi 
+# if [[ "$?" == "0" ]];then
+#     if [ ! -z "$GOPATH" ]; then
+#         echo "Add golang extension code "
+#         cp proto/extensions/go/*.ext $OUT_GRPC
+#     fi
+#     cd $OUT_GRPC
+#     # pwd 
+#     for f in *.go.ext; do
+#         mv "$f" "$( basename $f .ext )"
+#     done
+# else
+#     echo "Build error." 
+# fi 
