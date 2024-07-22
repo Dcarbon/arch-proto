611b0ea4dedd60ebaed38961b357ff3ac2029653
taink <taink317fwork@gmail.com>
2024-07-22 14:23:13 +0700

    update proto

--- scripts/build-go-grpc.sh
+++ scripts/build-go-grpc.sh
@@ -2,6 +2,7 @@ cd $( git rev-parse --show-toplevel )
 
 OUT_DIR="."
 OUT_GRPC="$OUT_DIR/pb"
+#OUT_DART="../panel/lib/pb"
 OUT_SWG="../swagger/json" 
 #OUT_SWG="$OUT_DIR/swagger"
 
@@ -11,7 +12,7 @@ OUT_SWG="../swagger/json"
 mkdir -p $OUT_GRPC
 mkdir -p $OUT_SWG
 # touch $OUT_SWG/aaa.json
-
+# --dart_out=$OUT_DART
 protoc -I . -I libs \
             --go_out=$OUT_DIR --go-grpc_out=$OUT_DIR  \
             --govalidators_out=$OUT_DIR \
